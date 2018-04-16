554ce4b14b0b48f81c1a5c6e75bc4837c3ee5c7c
Kevin M <math_b@live.fr>
2018-04-16 12:16:26 +0200

    add router

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
+import { SessionListComponent } from "@components/listerSession";
+
+import { router } from "./router";
+
 export default class Main {
-    constructor() {
-        alert('Conférence App démarré !');
-    }
+  constructor() {
+    alert("Conférence App démarré !");
+  }
 }
 
 let start = new Main();
+
+window.addEventListener("load", () => {
+  window.onhashchange = () => {
+    router();
+  };
+
+  router();
+});
---
+++ src/router.ts
@@ -0,0 +1,11 @@
+
+export const router = () => {
+    if (location.hash === "#speakers-list") {
+        // TODO afficher vue liste des présentateurs
+    } else if (location.hash === '#sessions-list') {
+        // TODO afficher vue liste des sessions
+    } else {
+        // TODO afficher vue par défaut
+    }
+}
+
